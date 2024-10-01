f3f55508a4ef461d21726cb8d9b98a2c1515319a
isam chajia <isamchajiabeat.com>
2024-10-01 09:58:19 +0100

    edit format mini prj 1

--- Day02/Mini project _1/function.c
+++ Day02/Mini project _1/function.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#define MAX_BOOKS  100
+#define MAX_BOOKS 100
 
 #include "function.h"
 
@@ -13,11 +13,9 @@ float prix[MAX_BOOKS];
 int quantities[MAX_BOOKS];
 int book_count = 0;
 
-
-
-
 // show menu :
-void show_menu(){
+void show_menu()
+{
     printf("\n---------------------------\n");
     printf("1-Ajouter un livre au stock. \n");
     printf("2-Afficher tous les livres disponibles. \n");
@@ -27,25 +25,25 @@ void show_menu(){
     printf("6-Afficher le nombre total de livres en stock. \n");
     printf("7-Exit. \n");
     printf("\n---------------------------\n");
-    
 }
 
 // add book :
-void add_book(){
+void add_book()
+{
     int i, n;
 
-    if ( book_count >= MAX_BOOKS)
+    if (book_count >= MAX_BOOKS)
     {
         printf("\n---------------------------\n");
         printf("Library is Full !!!!!.\n");
         return;
     }
-    
+
     printf("\n---------------------------\n");
     printf("How meny Book you wanna add : ");
     scanf("%d", &n);
 
-    for ( i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         getchar();
         // add title :
@@ -71,68 +69,65 @@ void add_book(){
 
         book_count++;
     }
-    
-
-    
 }
-
 
 // affichage :
 
-void affichage_book(){
+void affichage_book()
+{
 
     int i = 0;
-    if ( book_count == 0 )
+    if (book_count == 0)
     {
         system("cls");
         printf("No Book on library.\n");
         return;
     }
-    
-    for ( i = 0; i < book_count; i++)
+
+    for (i = 0; i < book_count; i++)
     {
         printf("\n---------------------------\n");
-        printf("Book %d ------------- \n", i+1);
+        printf("Book %d ------------- \n", i + 1);
         printf("Title : %s\n", titre[i]);
         printf("Autor : %s\n", autor[i]);
         printf("Price : %.2f\n", prix[i]);
         printf("Quantities : %d\n", quantities[i]);
     }
-    
 }
 
-
 // search :
-void search_book(){
+void search_book()
+{
     char search[50];
     bool found_book = false;
     int i;
-    if ( book_count == 0 )
+    if (book_count == 0)
     {
         printf("Library empty.\n");
         return;
     }
-    
+
     printf("\n----------------------\n");
     getchar();
     printf("searching About : ");
     scanf("%[^\n]", search);
 
-    for ( i = 0; i < book_count; i++)
+    for (i = 0; i < book_count; i++)
     {
-        if ( strcasecmp(search, titre[i]) == 0)
+        if (strcasecmp(search, titre[i]) == 0)
         {
             found_book = true;
             break;
-        }   
+        }
     }
-    if ( found_book == false )
+    if (found_book == false)
     {
         printf("Book not found\n");
-    }else if ( found_book == true )
+    }
+    else if (found_book == true)
     {
         printf("\n----------------------\n");
-        printf("The Book found in row : %d\n", i+1);
+        printf("The Book found in row : %d\n", i + 1);
         printf("Title : %s\n", titre[i]);
         printf("Autor : %s\n", autor[i]);
         printf("Price : %.2f\n", prix[i]);
@@ -140,10 +135,10 @@ void search_book(){
     }
 }
 
-
 // update quantities :
 
-void update_quantities(){
+void update_quantities()
+{
     char search_uq[50];
     bool found_book = false;
     int i, new_q;
@@ -153,18 +148,19 @@ void update_quantities(){
     printf("Enter Book Title for update the quantities : ");
     scanf("%[^\n]", search_uq);
 
-    for ( i = 0; i < book_count; i++)
+    for (i = 0; i < book_count; i++)
     {
-        if ( strcasecmp(search_uq, titre[i]) == 0)
+        if (strcasecmp(search_uq, titre[i]) == 0)
         {
             found_book = true;
             break;
-        }   
+        }
     }
-    if ( found_book == false )
+    if (found_book == false)
     {
         printf("Book not found\n");
-    }else if ( found_book == true )
+    }
+    else if (found_book == true)
     {
         printf("\n----------------------\n");
         printf("The Book is : \n");
@@ -178,14 +174,14 @@ void update_quantities(){
 
         quantities[i] = new_q;
 
-        printf("the new quantities of the book %s : %d", titre[i],quantities[i]);
-
+        printf("the new quantities of the book %s : %d", titre[i], quantities[i]);
     }
 }
 
 // Supprimer un Livre du Stock :
 
-void delet_book(){
+void delet_book()
+{
 
     char search_del[50];
     bool found_book = false;
@@ -196,19 +192,20 @@ void delet_book(){
     printf("Enter Book Title to Delet : ");
     scanf("%[^\n]", search_del);
 
-    for ( i = 0; i < book_count; i++)
+    for (i = 0; i < book_count; i++)
     {
-        if ( strcasecmp(search_del, titre[i]) == 0)
+        if (strcasecmp(search_del, titre[i]) == 0)
         {
             found_book = true;
 
             break;
-        }   
+        }
     }
-    if ( found_book == false )
+    if (found_book == false)
     {
         printf("Book not found\n");
-    }else if ( found_book == true )
+    }
+    else if (found_book == true)
     {
         printf("\n----------------------\n");
         printf("The Book is : \n");
@@ -217,25 +214,22 @@ void delet_book(){
         printf("Price : %.2f\n", prix[i]);
         printf("Quantities : %d\n", quantities[i]);
 
-
-        for ( i = i  ; i < book_count; i++)
+        for (i = i; i < book_count; i++)
         {
-            strcpy(titre[i], titre[i+1]);
-            strcpy(autor[i], autor[i+1]);
-            prix[i] = prix[i+1];
-            quantities[i] = quantities[i+1];
+            strcpy(titre[i], titre[i + 1]);
+            strcpy(autor[i], autor[i + 1]);
+            prix[i] = prix[i + 1];
+            quantities[i] = quantities[i + 1];
         }
 
         book_count--;
-        
     }
-
 }
 
-
 //  wait to click enter ;
-void wait_for_enter(){
+void wait_for_enter()
+{
     printf("\nPress Enter to show the menu...");
-    getchar();  
-    getchar();  
+    getchar();
+    getchar();
 }
